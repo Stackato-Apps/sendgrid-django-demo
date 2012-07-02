736d07adce8ede17da6578a829f523b38ad74096
Gabe Moothart <gmoothart@gmail.com>
2012-07-02 16:11:40 -0700

    added a 'real' unit test

--- settings.py
+++ settings.py
@@ -20,9 +20,10 @@ EMAIL_USE_TLS = True
 
 MANAGERS = ADMINS
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': '',                      # Or path to database file if using sqlite3.
         'USER': '',                      # Not used with sqlite3.
         'PASSWORD': '',                  # Not used with sqlite3.
@@ -134,6 +135,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'sg',
 )
 
 # A sample logging configuration. The only tangible logging
