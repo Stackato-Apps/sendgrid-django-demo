c287fd45f9925438287707106ec1c6eee6be0882
Steven Hsu <stevenhsu0@gmail.com>
2012-10-09 17:18:31 -0700

    using a middleware ConditionalGetMiddleware which will take care of setting the appropriatecontent-length header and value.

--- settings.py
+++ settings.py
@@ -98,6 +98,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
     # Uncomment the next line for simple clickjacking protection:
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.middleware.http.ConditionalGetMiddleware',
 )
 
 ROOT_URLCONF = 'sg.urls'
